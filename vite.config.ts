17454166b02c63a0af09ef6f80ca8927979fdb30
hoch12 <161817511+hoch12@users.noreply.github.com>
2026-03-24 08:34:04 +0100

    pre-deploy: prepare for GitHub Pages (HashRouter, vite base, gh-pages scripts)

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,8 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
+  base: "/coach/",
   server: {
     host: "::",
     port: 8080,
@@ -23,4 +24,4 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+});
